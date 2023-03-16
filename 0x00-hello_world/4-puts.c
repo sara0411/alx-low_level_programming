f93470e7e36bf92a93372d373f645c655b6a18dd
SARA <saraferraa0@gmaio.com>
2023-03-16 16:23:03 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#include <string.h>
-int main ()
+#include <stdio.h>
+
+/**
+ * main -this is the main function
+ *
+ * return - always 0
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
 }
